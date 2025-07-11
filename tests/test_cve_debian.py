aea99f77df29a4f6c1b6f874d2cd1511b6c3bbe7
Pedro <bartolomeip16@gmail.com>
2025-07-11 10:49:43 +0200

    Realigning CI to test proto and updatig requirements
    With some quickfixes to make pylint happy,
    and making CI run on all branches,
    and disabling the highly experimental "manual docker test"

--- tests/test_cve_debian.py
+++ tests/test_cve_debian.py
@@ -75,7 +75,9 @@ def test_get_cve_info_cve_2014_0160(wheezy_args):
 
 def test_get_cve_info_from_cached_json_file(bullseye_args):
     bullseye_args.cve_number = "2020-7247"
-    bullseye_args.cache_main_json_file = "tests/test-material/opensmtpd-json-cached-data"
+    bullseye_args.cache_main_json_file = (
+        "tests/test-material/opensmtpd-json-cached-data"
+    )
     results = get_cve_details_from_json(bullseye_args)
     assert len(results) == 1
     assert results[0]["src_package"] == "opensmtpd"
